43ad45dcecb8e6a246aa50db9c0e5c8ea1844671
Carlos Ferreira <carloshsf3899@gmail.com>
2022-03-17 09:36:45 +0000

    Move main script outside of functions Clean csv read code Redefine records as a list of dicts with str keys and str values

--- Medical_Records/scripts/main.py
+++ Medical_Records/scripts/main.py
@@ -1,60 +1,59 @@
 import re
-from records import Records,MedicalRecord
-import yeardist
+import sys
+from typing import List, Dict
+# from records import Records,MedicalRecord
+# import yeardist
 
-def readCSV(filename:str) -> Records:
-    with open(filename, "r") as fp:
-        records = {}
-        pattern = re.compile(r"""
-            ^                            # start of string
-            (?P<id>\w+),                 # _id
-            (?P<index>\d+),              # index
-            (?P<date>\d{4}-\d{2}-\d{2}), # dataEMD
-            (?P<firstname>\w+),          # nome/primeiro
-            (?P<lastname>\w+),           # nome/último
-            (?P<age>[1-9]\d?),           # idade
-            (?P<gender>[FM]),            # género
-            (?P<city>\w+),               # morada
-            (?P<sport>\w+),              # modalidade
-            (?P<club>\w+),               # clube
-            (?P<email>[^,]+),            # email
-            (?P<fed>true|false),         # federado
-            (?P<result>true|false)       # resultado
-            $                            # end of string
-        """, re.X)
-        for line in fp.readlines():
-            match = pattern.match(line)
-            if match:
-                entry = MedicalRecord(match.groupdict())
-                records[entry.data["id"]] = entry
-        print("$!> CSV FILE HAS BEEN READ!")
-        return records
+Records = List[Dict[str, str]]
+records: Records = []
 
-def writeHTML_records(records:Records,filename:str):
-    with open(filename,"w") as fp:
-        sortDate = list(records.values())
-        sortDate.sort(reverse=True)
-        for record in sortDate:
-            fp.write(record.markupify())
-        print(f'$!> Records written to {filename}')
+pattern = re.compile(r"""
+    ^                            # start of string
+    (?P<id>\w+),                 # _id
+    (?P<index>\d+),              # index
+    (?P<date>\d{4}-\d{2}-\d{2}), # dataEMD
+    (?P<firstname>\w+),          # nome/primeiro
+    (?P<lastname>\w+),           # nome/último
+    (?P<age>[1-9]\d?),           # idade
+    (?P<gender>[FM]),            # género
+    (?P<city>\w+),               # morada
+    (?P<sport>\w+),              # modalidade
+    (?P<club>\w+),               # clube
+    (?P<email>[^,]+),            # email
+    (?P<fed>true|false),         # federado
+    (?P<result>true|false)       # resultado
+    $                            # end of string
+""", re.X)
 
-def printRecords(records:Records):
-    for record in records.values():
-        print(record)
-    print(f'{records.__len__()} total records')
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        if match := pattern.match(line):
+            records.append(match.groupdict())
 
-
-records = readCSV("../emd.csv")
-
-#printRecords(records)
-writeHTML_records(records, "list.html")
-
-queryB = yeardist.generate(records,"gender") 
-queryC = yeardist.generate(records,"sport") 
-queryF = yeardist.generate(records,"fed") 
-queryG = yeardist.generate(records,"result") 
-
-yeardist.writeHTML(*queryB)
-yeardist.writeHTML(*queryC)
-yeardist.writeHTML(*queryF)
-yeardist.writeHTML(*queryG)
+# def writeHTML_records(records: List[Dict[str, str]], filename: str):
+#     with open(filename, "w") as fp:
+#         sortDate = list(records.values())
+#         sortDate.sort(reverse=True)
+#         for record in sortDate:
+#             fp.write(record.markupify())
+#         print(f'$!> Records written to {filename}')
+#
+# def printRecords(records: Records):
+#     for record in records.values():
+#         print(record)
+#     print(f'{len(records)} total records')
+#
+# records = readCSV("../emd.csv")
+#
+# #printRecords(records)
+# writeHTML_records(records, "list.html")
+#
+# queryB = yeardist.generate(records, "gender")
+# queryC = yeardist.generate(records, "sport")
+# queryF = yeardist.generate(records, "fed")
+# queryG = yeardist.generate(records, "result")
+#
+# yeardist.writeHTML(*queryB)
+# yeardist.writeHTML(*queryC)
+# yeardist.writeHTML(*queryF)
+# yeardist.writeHTML(*queryG)
