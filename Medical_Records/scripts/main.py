2cbbb956dfe5926d62689ef3f579eed116dda89d
Carlos Ferreira <carloshsf3899@gmail.com>
2022-03-21 21:01:55 +0000

    Move csv read code to readCSV() function

--- Medical_Records/scripts/main.py
+++ Medical_Records/scripts/main.py
@@ -5,33 +5,7 @@ import yeardist
 from records import Records, markupify, writeHTML_records
 
 def main() -> int:
-    records: Records = []
-
-    pattern = re.compile(r"""
-        ^                            # start of string
-        (?P<id>\w+),                 # _id
-        (?P<index>\d+),              # index
-        (?P<date>\d{4}-\d{2}-\d{2}), # dataEMD
-        (?P<firstname>\w+),          # nome/primeiro
-        (?P<lastname>\w+),           # nome/último
-        (?P<age>[1-9]\d?),           # idade
-        (?P<gender>[FM]),            # género
-        (?P<city>\w+),               # morada
-        (?P<sport>\w+),              # modalidade
-        (?P<club>\w+),               # clube
-        (?P<email>[^,]+),            # email
-        (?P<fed>true|false),         # federado
-        (?P<result>true|false)       # resultado
-        $                            # end of string
-    """, re.X)
-
-    # read csv
-    # TODO: move to function
-    with open(sys.argv[1], 'r') as f:
-        next(f)
-        for line in f:
-            if match := pattern.match(line):
-                records.append(match.groupdict())
+    records: Records = readCSV(sys.argv[1])
 
     # Test prints - show if 300 records have been correctly parsed and validated
     # !!! Remove before submitting
@@ -56,6 +30,35 @@ def main() -> int:
     yeardist.writeHTML(*queryG)
 
     return 0
+
+def readCSV(csv_file: str) -> Records:
+    records: Records = []
+
+    pattern = re.compile(r"""
+        ^                            # start of string
+        (?P<id>\w+),                 # _id
+        (?P<index>\d+),              # index
+        (?P<date>\d{4}-\d{2}-\d{2}), # dataEMD
+        (?P<firstname>\w+),          # nome/primeiro
+        (?P<lastname>\w+),           # nome/último
+        (?P<age>[1-9]\d?),           # idade
+        (?P<gender>[FM]),            # género
+        (?P<city>\w+),               # morada
+        (?P<sport>\w+),              # modalidade
+        (?P<club>\w+),               # clube
+        (?P<email>[^,]+),            # email
+        (?P<fed>true|false),         # federado
+        (?P<result>true|false)       # resultado
+        $                            # end of string
+    """, re.X)
+
+    with open(csv_file, 'r') as f:
+        next(f)
+        for line in f:
+            if match := pattern.match(line):
+                records.append(match.groupdict())
+
+    return records
 
 if __name__ == '__main__':
     sys.exit(main())
