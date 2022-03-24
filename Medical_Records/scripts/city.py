00305a5400711ed15de69afd908666edbef39275
Carlos Ferreira <carloshsf3899@gmail.com>
2022-03-24 22:45:46 +0000

    Improve city function

--- Medical_Records/scripts/city.py
+++ Medical_Records/scripts/city.py
@@ -6,11 +6,15 @@ from typing import Any, Dict, List
 City = Dict[str, List[Dict[str, Any]]]
 
 def city(records: Records) -> City:
-    grouped_by_city: City = {}
-    city = itemgetter('city')
-    for k, g in groupby(sorted(records, key=city), key=city):
-        grouped_by_city[k] = list(g)
-    return grouped_by_city
+    f = itemgetter('city')
+    return {k: [*g] for k, g in groupby(sorted(records, key=f), key=f)}
+    # return {k: [*g] for k, g in groupby(sorted(records, key=itemgetter('city')), key=itemgetter('city'))}
+    # return dict(map(lambda kg: (kg[0], list(kg[1])), groupby(sorted(records, key=itemgetter('city')), key=itemgetter('city'))))
+    # grouped_by_city: City = {}
+    # city = itemgetter('city')
+    # for k, g in groupby(sorted(records, key=city), key=city):
+    #     grouped_by_city[k] = list(g)
+    # return grouped_by_city
 
 def dist_city(city: City) -> Dict[str, int]:
     return {k: len(v) for k, v in city.items()}
