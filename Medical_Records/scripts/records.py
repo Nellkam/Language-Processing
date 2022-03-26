eba6f1352a1fe5eaca5f0845dc84bc1520cee88f
CarlosHSF <44176623+CarlosHSF99@users.noreply.github.com>
2022-03-26 22:35:05 +0000

    Jinja Templates (#13)
    * Add index.html template
    
    * Add records.html template
    
    * Fix records.html template
    
    * Add query names to index.html
    
    * Move template directory inside scripts for ease of use
    Modify writeHTML_records to use jinja2
    Remove markupify

--- Medical_Records/scripts/records.py
+++ Medical_Records/scripts/records.py
@@ -5,22 +5,8 @@ from inspect import cleandoc
 Record = Dict[str, str]
 Records = List[Record]
 
-def markupify(record: Record) -> str:
-    return cleandoc(f"""
-        <h2>{record['id']} [{record['date']}]</h2>
-        <ul>
-        \t<li>[{record['index']}] {record['lastname']},{record['firstname']}</li>
-        \t<li>{'Male' if record['gender'] == 'M' else 'Female'}, {record['age']} years old</li>
-        \t<li>{record['email']}</li>
-        \t<li>Lives in {record['city']}</li>
-        \t<li>{record['sport']} for {record['club']}</li>
-        \t<li>Federated: {'Yes' if record['fed'] == 'true' else 'No'}</li>
-        \t<li>{'Positive' if record['result'] == 'true' else 'Negative'}</li>
-        </ul>
-    """)
-
-def writeHTML_records(records: Records, filename: str):
-    records_by_date = sorted(records, key=itemgetter('date'), reverse=True)
-    with open(filename, "w") as f:
-        for record in records_by_date:
-            f.write(f"{markupify(record)}\n")
+def writeHTML_records(env, records: Records):
+    records_by_name = sorted(records, key=itemgetter('firstname', 'lastname'), reverse=True)
+    template = env.get_template('records.html')
+    with open('records.html', 'w') as f:
+        f.write(template.render(records=records))
